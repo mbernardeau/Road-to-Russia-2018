6240239e54f308ed002bfa7d0bfeee9a0398994e
Mathias Bernardeau <mathias.bernardeau-ext@vif.fr>
2017-07-20 16:43:57 +0200

    Update function to only register lastModified if not recently modified

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-// const functions = require('firebase-functions');
+const functions = require('firebase-functions');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,5 +7,11 @@
 //  response.send("Hello from Firebase!");
 // });
 
-// exports.onBetUpdated = functions.database.ref('/bets/{matchId}/users/{uid}')
-//   .onWrite((event) => event.data.ref.child('lastModified').set(new Date().getTime()));
+exports.onBetUpdated = functions.database.ref('/bets/{matchId}/users/{uid}')
+  .onWrite((event) =>
+    event.data.ref.child('lastModified').once('value', (snap) => { // eslint-disable-line consistent-return
+      if (!snap.exists() || event.timestamp - snap.val() > 200) {
+        return event.data.ref.child('lastModified').set(event.timestamp);
+      }
+    })
+);
