5170e5bc2242ca53cb768c82efba8a50886c2660
Mathias Bernardeau <mathias.bernardeau-ext@vif.fr>
2017-07-20 17:06:06 +0200

    Upgrade comments of cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,15 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
+/**
+ * Function that automatically stores date of last modification of a bet, triggered by write event on a bet node
+ */
 exports.onBetUpdated = functions.database.ref('/bets/{matchId}/users/{uid}')
   .onWrite((event) =>
-    event.data.ref.child('lastModified').once('value', (snap) => { // eslint-disable-line consistent-return
-      if (!snap.exists() || event.timestamp - snap.val() > 200) {
-        return event.data.ref.child('lastModified').set(event.timestamp);
-      }
-    })
+    event.data.ref.child('lastModified').once('value', (snap) =>
+         !snap.exists() && // Only perform update if lastModified node does not exist
+         event.data.ref.child('lastModified').set(event.timestamp)
+    )
 );
