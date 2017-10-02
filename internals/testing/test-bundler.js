08c271cbe7491bd20469decbc28340a6d77c19a8
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2017-10-02 19:11:29 +0200

    Remove test warning

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,3 +1,7 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import 'babel-polyfill';
+
+global.requestAnimationFrame = (callback) => {
+  setTimeout(callback, 0);
+};
