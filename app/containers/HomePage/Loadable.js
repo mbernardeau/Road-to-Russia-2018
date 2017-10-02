1bce9de5d4993249ee76af587d0e5bd75b0e8906
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2017-10-02 14:24:53 +0200

    Fix unknooxn props error

--- app/containers/HomePage/Loadable.js
+++ app/containers/HomePage/Loadable.js
@@ -1,10 +1,11 @@
 /**
  * Asynchronously loads the component for HomePage
  */
+import React from 'react';
 import Loadable from 'react-loadable';
 import { CircularProgress } from 'material-ui/Progress';
 
 export default Loadable({
   loader: () => import('./index'),
-  loading: CircularProgress,
+  loading: () => <CircularProgress />,
 });
