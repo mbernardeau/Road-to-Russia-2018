f96c651b069f45cf3ec48f03bcb029eb2ea5236b
naustra <lebihanp@hotmail.fr>
2018-05-10 13:34:11 +0200

    Creation de la page de validation de l'inscription

---
+++ app/screens/ValidInscription/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class ValidInscription extends React.PureComponent {
+  render() {
+    return <h1>Ceci sera la page de validation des inscriptions.</h1>
+  }
+}
