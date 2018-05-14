57b42a36e7c79a12546b6ed1f6e0280d27cce5db
naustra <lebihanp@hotmail.fr>
2018-05-14 19:09:00 +0200

    Update FAQ (#191)
    * Update FAQ
    
    * Add gestion FaqEntry in FAQ

--- app/screens/FAQ/FAQ.jsx
+++ app/screens/FAQ/FAQ.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import ExpansionPanel, {
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-} from 'material-ui/ExpansionPanel'
 import Typography from 'material-ui/Typography'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+
+import FaqEntry from './FaqEntry'
 
 import './FAQ.scss'
 
@@ -14,19 +11,47 @@ export default class FAQPage extends React.PureComponent {
     return (
       <div className="faq-page-div">
         <p className="faq-speech">
-          Cette page référence les questions fréquentes que vous pourrez vous posez sur l&apos;utilisation du site
+          <Typography>
+            Cette page référence les questions fréquentes que vous pourrez vous posez sur
+            l&apos;utilisation du site
+          </Typography>
         </p>
-        <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className="faq-question">Comment fait-on pour changer de compte Facebook ou Google ?</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography className="faq-response">
-              Il faut changer de compte Google ou Facebook avec le navigateur ou le smartphone avant de se connecter sur le
-              site internet ou l&apos;application. 
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
+        <FaqEntry
+          question="Qu’est-ce que c’est ?"
+          answer="Un site qui vous permet de jouer avec les pronostics du Mondial, les trois premiers de chaque tribu se verra attribuer une récompense à la fin de la Coupe du Monde."
+        />
+        <FaqEntry
+          question="Comment participer ?"
+          answer="Créer votre tribu via l&apos;onglet correspondant, entrez votre nom d’équipe, invitez
+          vos proches, et vous voilà prêts à pronostiquer sur l&apos;issue des matchs."
+        />
+        <FaqEntry
+          question="Paiement Obligatoire ?"
+          answer="Dans l&apos;absolu, non. Vous pouvez jouer sans miser, cependant vous ne participerez
+          pas à l&apos;élection de la tribu gagnante pour remporter le prix. Chaque Tribu fixe
+          son prix d’entrée, et le chef de tribu choisi comment les gains seront redistribués
+          aux membres de son équipe."
+        />
+        <FaqEntry
+          question="Que remporte le tribu gagnant ?"
+          answer="Déduction faite des dons à l’association PAM, le tribu gagnant se verra congratulé de
+          la moitié des gains misés par la totalité des autres participants."
+        />
+        <FaqEntry
+          question="Comment créer une Tribu ?"
+          answer="Via page page dédiée à cet effet. Entrez votre nom d&apos;équipe, le montant que vous
+          souhaitez miser par personne, et bienvenue dans l&apos;aventure."
+        />
+        <FaqEntry
+          question="Comment rejoindre une Tribu ?"
+          answer="Le chef de tribu recevra une notification sur votre demande. Une fois accepté, vous
+          recevrez un code afin de vous acquitter du montant demandé pour rejoindre le groupe."
+        />
+        <FaqEntry
+          question="Comment fait-on pour changer de compte Facebook ou Google ?"
+          answer="Il faut changer de compte Google ou Facebook avec le navigateur ou le smartphone avant
+          de se connecter sur le site internet ou l&apos;application."
+        />
       </div>
     )
   }
