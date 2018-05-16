41ffea7921dd7ff563e8d82d3ac3119276b0e0de
naustra <lebihanp@hotmail.fr>
2018-05-16 19:55:12 +0200

    Adapt for material-ui 1.0

--- app/screens/Winner/Winner.jsx
+++ app/screens/Winner/Winner.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import Card, { CardContent } from 'material-ui/Card'
-import Typography from 'material-ui/Typography'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
 
 import WinnerChoice from './WinnerChoice'
 
